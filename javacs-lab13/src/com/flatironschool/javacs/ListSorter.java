46ec6724620e7631842ed6f27633ee75b0ee997d
Ajene Johnson <ajenejohnson@ajenes-mbp.attlocal.net>
2016-07-25 14:44:12 -0700

    Done.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -9,7 +9,7 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
-
+import java.util.Collections;
 /**
  * Provides sorting algorithms.
  *
@@ -63,8 +63,39 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        
+        int size = list.size();
+        List<T> firstHalf = list.subList(0, size/2);
+        List<T> secondHalf = list.subList(size/2, size);
+        List<T> sortedList = new ArrayList<T>(size);
+        Collections.sort(firstHalf, comparator);
+        Collections.sort(secondHalf, comparator);
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int fsize = firstHalf.size();
+        int ssize = secondHalf.size();
+
+        while(i < fsize && j < ssize && k < size){
+        	
+        	if(comparator.compare(firstHalf.get(i), secondHalf.get(j)) <= 0){
+        		sortedList.add(k, firstHalf.get(i));
+        		i++;
+        		k++;
+        		
+        	}else if(i == fsize-1 && j == ssize-1){
+        		sortedList.add(k, secondHalf.get(j));
+        		k++;
+        		sortedList.add(k, firstHalf.get(i));
+        		k++;
+        		i++;
+        	}else{
+        		sortedList.add(k, secondHalf.get(j));
+        		j++;
+        		k++;
+        	}
+		}
+        return sortedList;
 	}
 
 	/**
@@ -75,7 +106,21 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+        
+        List<T> sorted = heapSortHelper(list, comparator);
+		list.clear();
+		list.addAll(sorted);
+        
+	}
+
+	public List<T> heapSortHelper(List<T> list, Comparator<T> comparator){
+		PriorityQueue<T> minHeap = new PriorityQueue<T>(list);
+        T[] obj = (T[]) minHeap.toArray();
+        List<T> sortedList = new ArrayList<T>();
+        for(T object: obj){
+        	sortedList.add(object);
+        }
+        return sortedList;
 	}
 
 	
@@ -89,8 +134,32 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        
+        PriorityQueue<T> minHeap = new PriorityQueue<T>(k, comparator);
+	     
+	    for(int i =0; i < list.size(); i++){
+	    	
+	        if(minHeap.size() < k){
+	        	minHeap.offer(list.get(i));
+	        }else if(comparator.compare(list.get(i), minHeap.peek()) <= 0){
+	        	continue;
+	        }else if(comparator.compare(list.get(i), minHeap.peek()) > 0){
+	        	minHeap.poll();
+	        	minHeap.offer(list.get(i));
+	        	minHeap.peek();
+	        }
+    	}
+
+    	list.clear();
+        
+        T[] obj = (T[]) minHeap.toArray();
+        
+        for(T object: obj){
+        	list.add(object);
+        }
+        
+        Collections.sort(list, comparator);
+        return list;
 	}
 
 	
